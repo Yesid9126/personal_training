712b76a377f63a311436a2e8cb05d9cd097489f0
Yesid9126 <yesidf26@gmail.com>
2023-09-04 21:43:43 -0500

    init project

---
+++ personal_training/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from personal_training.users.models import User
+from personal_training.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
